669d76a2271e759e2bf2466ff2c7c1b6347c479a
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-05-13 01:49:02 -0700

    update cwu color admin index.php

--- QuizCore/admin-temp/index.php
+++ QuizCore/admin-temp/index.php
@@ -30,7 +30,21 @@ $count112 = 0;
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
 
   <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <style> 
+  .bg-red {
+      background-color: #ab0032;
+      /* Your desired background color */
+    }
 
+    .nav-link {
+      color:black;
+    }
+
+    .nav {
+    --bs-nav-link-hover-color: #ab0032;
+
+}
+    </style>
   <style>
     .bd-placeholder-img {
       font-size: 1.125rem;
@@ -63,10 +77,9 @@ $count112 = 0;
     }
 
     .bi {
-      vertical-align: -0.125em;
-      fill: #ab0433;
-      width: 30%;
-    }
+        vertical-align: -0.125em;
+        fill: currentColor;
+      }
 
     .nav-scroller {
       position: relative;
@@ -114,7 +127,7 @@ $count112 = 0;
   <!-- Custom styles for this template -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet" />
   <!-- Custom styles for this template -->
-  <link href="css/dashboard.css" rel="stylesheet" />
+  <link href="./css/dashboard.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -228,11 +241,11 @@ $count112 = 0;
     </symbol>
   </svg>
 
-  <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="index.php">
-      <img src="../img/cwu-wildcat-spirit-mark-single-color-reversed.png" alt="Logo" style="height: 40px" />
-    </a>
-
+  <header class="navbar sticky-top bg-red flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+  <a class="navbar-brand me-0 px-3 fs-6" href="index">
+    <img src="../img/cwu-wildcat-spirit-mark-single-color-reversed.png" alt="Logo" style="height: 40px" />
+  </a>
+  
     <ul class="navbar-nav flex-row d-md-none">
       <li class="nav-item text-nowrap">
         <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
