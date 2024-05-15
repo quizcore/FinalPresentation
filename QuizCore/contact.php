a5c3e1d1ae3ae2bec9f9a2565016e1c2e7732f43
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-05-15 15:43:43 -0700

    update contact alert

--- QuizCore/contact.php
+++ QuizCore/contact.php
@@ -1,32 +1,34 @@
 <?php
-	session_start();
-	
-	$conn = mysqli_connect($_SESSION['servername'], $_SESSION['username'], $_SESSION['password'], $_SESSION['dbname']);
-	
-	// Check if the send button was pressed
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		$name = test_input($_POST['name']);
-		$email = test_input($_POST['contactEmail']);
-		$body = test_input($_POST['emailBody']);
-		
-		if (strlen($name) > 0 && strlen($email) > 0  && strlen($body) > 0) {
-			$sql = "INSERT INTO contact(contact_name, contact_email, contact_message) VALUES('$name', '$email', '$body');";
-			mysqli_query($conn, $sql);
-		}
-	}
-	
-	function test_input($data) {
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-	
-	$pageTitle = "Contact Us";
-	require_once 'header.php';
+session_start();
+
+$conn = mysqli_connect($_SESSION['servername'], $_SESSION['username'], $_SESSION['password'], $_SESSION['dbname']);
+
+// Check if the send button was pressed
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST['name']);
+  $email = test_input($_POST['contactEmail']);
+  $body = test_input($_POST['emailBody']);
+
+  if (strlen($name) > 0 && strlen($email) > 0  && strlen($body) > 0) {
+    $sql = "INSERT INTO contact(contact_name, contact_email, contact_message) VALUES('$name', '$email', '$body');";
+    mysqli_query($conn, $sql);
+
+    // Success message after successful insert using alert
+    echo "<script>alert('Your message has been sent successfully!');</script>";
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$pageTitle = "Contact Us";
+require_once 'header.php';
 ?>
 
-<!--Main-->
 <div class="container">
   <h1>Contact Us</h1>
   <p>
@@ -46,13 +48,11 @@
       <textarea class="form-control" id="message" name="emailBody" rows="5" required></textarea>
     </div>
     <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-      <!-- Submit button (Add back end code to save contact info) -->
       <input type="submit" class="btn btn-bd-red btn-lg px-4 me-md-2" value="Send">
     </div>
   </form>
-</div>
-<!--End of Main-->
 
+</div>
 <?php
 // Include footer.
 require_once 'footer.php';
