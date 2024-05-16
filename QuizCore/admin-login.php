a6554d6cc990dc3e0cc38ef03fcb1e03b8d21440
KhangBBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-05-16 15:49:10 -0700

    Refactor code

--- QuizCore/admin-login.php
+++ QuizCore/admin-login.php
@@ -32,31 +32,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $password = trim($_POST["password"]);
     }
-	
-	// Check for valid login based on database entries
-	$valid = False;
+
+    // Check for valid login based on database entries
+    $valid = False;
 
     // Validate credentials.
     if (empty($email_err) && empty($password_err)) {
-		while($row = $result->fetch_assoc()) {
-			if($row["email"] === $email && $row["password"] === $password) {
-				// Password is correct, start a new session.
-				session_start();
-				$valid = True;
-				
-				// Store data in session variables.
-				$_SESSION["loggedin"] = true;
-				$_SESSION["admin_email"] = $email;
+        while ($row = $result->fetch_assoc()) {
+            if ($row["email"] === $email && $row["password"] === $password) {
+                // Password is correct, start a new session.
+                session_start();
+                $valid = True;
 
-				// Redirect user to admin page.
-				header("location: /admin-temp/admin.php");
-			}
+                // Store data in session variables.
+                $_SESSION["loggedin"] = true;
+                $_SESSION["admin_email"] = $email;
+
+                // Redirect user to admin page.
+                header("location: /admin-temp/admin.php");
+            }
         }
-		if($valud === False) {
-			// Email doesn't exist, display a generic error message.
-			$login_err = "Invalid email or password.";
-			echo "<script>alert(`$login_err`)</script>";
-		}
+        if ($valud === False) {
+            // Email doesn't exist, display a generic error message.
+            $login_err = "Invalid email or password.";
+            echo "<script>alert(`$login_err`)</script>";
+        }
     } else {
         $login_err = $email_err . " " . $password_err;
         echo "<script>alert(`$login_err`)</script>";
