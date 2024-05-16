81bc60be514c48c14ef1343f933bcf8e5486a046
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-05-16 14:16:29 -0700

    update icons

--- QuizCore/admin-temp/all-students.php
+++ QuizCore/admin-temp/all-students.php
@@ -20,7 +20,7 @@ require_once 'header.php';
 <!--main-->
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">All of all students</h1>
+    <h1 class="h2">List of all students</h1>
   </div>
 
   <!--Data table-->
