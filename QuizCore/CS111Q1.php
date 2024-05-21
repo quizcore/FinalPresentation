82a5462a868df110492e6407e808edb21970129d
tuho9428 <thanhtu10c9@gmail.com>
2024-05-20 21:19:12 -0700

    add progress bar 111q1

--- QuizCore/CS111Q1.php
+++ QuizCore/CS111Q1.php
@@ -42,12 +42,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $pageTitle = "Exam";
 require_once 'testheader.php';
+// Include functions file
+require_once 'functions.php';
 ?>
 <!--Main-->
 <!--Main Div-->
 <div class="container shadow p-3 my-5 bg-body-tertiary rounded">
 	<h2>Exam Question Set: 4</h2>
 </div>
+
+<!-- Progress bar -->
+<?php echo generateProgressBar(20); ?>
 
 <div class="container">
 	<!--Questions pulled from database-->
