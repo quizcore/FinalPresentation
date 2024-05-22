ecca8dca87adaf8970bb91484e229af298e41e18
KhangBBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-05-22 01:26:37 -0700

    Update datatable dep links

--- QuizCore/admin-temp/header.php
+++ QuizCore/admin-temp/header.php
@@ -17,9 +17,8 @@
 
   <!-- data table -->
   <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
-  <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js" crossorigin="anonymous"></script>
-  <script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.7/css/dataTables.bootstrap5.css" crossorigin="anonymous">
+  <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-2.0.7/datatables.min.css" rel="stylesheet">
+  <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-2.0.7/datatables.min.js"></script>
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- bootstrap icon -->
