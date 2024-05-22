68905a7dfd9ac30e94e484966480e456de541196
KhangBBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-05-21 21:11:13 -0700

    Reformat color-mode.js

--- QuizCore/assets/js/color-modes.js
+++ QuizCore/assets/js/color-modes.js
@@ -18,137 +18,136 @@
 
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
-  
-//getLocalSunsetTime start
-const getLocalSunsetTime = () => {
-  return new Promise((resolve, reject) => {
-      const geoSuccess = (position) => {
-          const { latitude, longitude } = position.coords;
-          const date = new Date();
-          const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 
-          fetch(`https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=America/Los_Angeles&date=${formattedDate}`)
+  //getLocalSunsetTime start
+  const getLocalSunsetTime = () => {
+    return new Promise((resolve, reject) => {
+      const geoSuccess = (position) => {
+        const { latitude, longitude } = position.coords;
+        const date = new Date();
+        const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+
+        fetch(`https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=America/Los_Angeles&date=${formattedDate}`)
           //https://api.sunrisesunset.io/json?lat=38.907192&lng=-77.036873&timezone=UTC&date=1990-05-22
-              .then(response => response.json())
-              .then(data => {
-                  const sunsetTime = data.results.sunset;
-                  console.log('sunsetTime:', sunsetTime);
-                  // Split the sunset time into hours and minutes
-                  const [hourStr, minuteStr] = sunsetTime.split(':');
-                  let sunsetHour = parseInt(hourStr); // Extract hour value
-                  const minutes = parseInt(minuteStr); // Extract minute value
-                  
-                  // Adjust sunsetHour if it's PM (afternoon)
-                  if (sunsetTime.includes('PM') && sunsetHour !== 12) {
-                      sunsetHour += 12;
-                  }
-                  
-                  console.log('sunsetHour:', sunsetHour);
-                  console.log('current hours:', date.getHours());
-                  if (!isNaN(sunsetHour)) {
-                      resolve(sunsetHour);
-                  } else {
-                      reject('Invalid sunset time');
-                  }
-              })
-              .catch(error => reject(error));
+          .then(response => response.json())
+          .then(data => {
+            const sunsetTime = data.results.sunset;
+            console.log('sunsetTime:', sunsetTime);
+            // Split the sunset time into hours and minutes
+            const [hourStr, minuteStr] = sunsetTime.split(':');
+            let sunsetHour = parseInt(hourStr); // Extract hour value
+            const minutes = parseInt(minuteStr); // Extract minute value
+
+            // Adjust sunsetHour if it's PM (afternoon)
+            if (sunsetTime.includes('PM') && sunsetHour !== 12) {
+              sunsetHour += 12;
+            }
+
+            console.log('sunsetHour:', sunsetHour);
+            console.log('current hours:', date.getHours());
+            if (!isNaN(sunsetHour)) {
+              resolve(sunsetHour);
+            } else {
+              reject('Invalid sunset time');
+            }
+          })
+          .catch(error => reject(error));
       };
 
       const geoError = (error) => {
-          reject(error);
+        reject(error);
       };
 
       if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
       } else {
-          reject('Geolocation is not supported by this browser.');
+        reject('Geolocation is not supported by this browser.');
       }
-  });
-};
-//getLocalSunsetTime end
+    });
+  };
+  //getLocalSunsetTime end
 
-//getLocalSunriseTime start
-const getLocalSunriseTime = () => {
-  return new Promise((resolve, reject) => {
+  //getLocalSunriseTime start
+  const getLocalSunriseTime = () => {
+    return new Promise((resolve, reject) => {
       const geoSuccess = (position) => {
-          const { latitude, longitude } = position.coords;
-          const date = new Date();
-          const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+        const { latitude, longitude } = position.coords;
+        const date = new Date();
+        const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 
-          fetch(`https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=America/Los_Angeles&date=${formattedDate}`)
-              .then(response => response.json())
-              .then(data => {
-                  const sunriseTime = data.results.sunrise;
-                  console.log('sunriseTime:', sunriseTime);
-                  const sunriseHour = parseInt(sunriseTime.split(':')[0]);
-                  console.log('sunriseHour:', sunriseHour);
-                  console.log('current hours:', date.getHours());
-                  if (!isNaN(sunriseHour)) {
-                      resolve(sunriseHour);
-                  } else {
-                      reject('Invalid sunrise time');
-                  }
-              })
-              .catch(error => reject(error));
+        fetch(`https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=America/Los_Angeles&date=${formattedDate}`)
+          .then(response => response.json())
+          .then(data => {
+            const sunriseTime = data.results.sunrise;
+            console.log('sunriseTime:', sunriseTime);
+            const sunriseHour = parseInt(sunriseTime.split(':')[0]);
+            console.log('sunriseHour:', sunriseHour);
+            console.log('current hours:', date.getHours());
+            if (!isNaN(sunriseHour)) {
+              resolve(sunriseHour);
+            } else {
+              reject('Invalid sunrise time');
+            }
+          })
+          .catch(error => reject(error));
       };
 
       const geoError = (error) => {
-          reject(error);
+        reject(error);
       };
 
       if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
       } else {
-          reject('Geolocation is not supported by this browser.');
+        reject('Geolocation is not supported by this browser.');
       }
-  });
-};
-//getLocalSunriseTime end
+    });
+  };
+  //getLocalSunriseTime end
 
-const setTheme = theme => {
-  const signUpBtn = document.getElementById('signUpBtn');
+  const setTheme = theme => {
+    const signUpBtn = document.getElementById('signUpBtn');
 
-  if (signUpBtn) {
-    if (theme === 'dark') {
+    if (signUpBtn) {
+      if (theme === 'dark') {
         signUpBtn.classList.add('btn-bd-red');
-    } else {
+      } else {
         signUpBtn.classList.remove('btn-bd-red');
+      }
     }
-  }
 
-  if (theme === 'auto') {
+    if (theme === 'auto') {
       Promise.all([getLocalSunsetTime(), getLocalSunriseTime()]).then(([sunsetHour, sunriseHour]) => {
-          const currentHour = new Date().getHours();
+        const currentHour = new Date().getHours();
 
-          if ((currentHour >= sunsetHour || currentHour < sunriseHour)) {
-              
-              if (signUpBtn) {
-                  signUpBtn.classList.add('btn-bd-red');
-              }
-              document.documentElement.setAttribute('data-bs-theme', 'dark');
-          } else {
-              
-              if (signUpBtn) {
-                  signUpBtn.classList.remove('btn-bd-red');
-              }
-              document.documentElement.setAttribute('data-bs-theme', 'light');
+        if ((currentHour >= sunsetHour || currentHour < sunriseHour)) {
+
+          if (signUpBtn) {
+            signUpBtn.classList.add('btn-bd-red');
           }
+          document.documentElement.setAttribute('data-bs-theme', 'dark');
+        } else {
+
+          if (signUpBtn) {
+            signUpBtn.classList.remove('btn-bd-red');
+          }
+          document.documentElement.setAttribute('data-bs-theme', 'light');
+        }
       }).catch(error => {
-          console.error('Error fetching sunrise/sunset time:', error);
+        console.error('Error fetching sunrise/sunset time:', error);
       });
-  } else {
+    } else {
       document.documentElement.setAttribute('data-bs-theme', theme);
       if (signUpBtn) {
-          if (theme === 'dark') {
-              signUpBtn.classList.add('btn-bd-red');
-          } else {
-              signUpBtn.classList.remove('btn-bd-red');
-          }
+        if (theme === 'dark') {
+          signUpBtn.classList.add('btn-bd-red');
+        } else {
+          signUpBtn.classList.remove('btn-bd-red');
+        }
       }
+    }
+
   }
-
-}
-
 
   setTheme(getPreferredTheme())
 
@@ -211,9 +210,3 @@ const setTheme = theme => {
       })
   })
 })()
-
-
-
-
-
-
