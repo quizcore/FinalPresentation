fca8763eda51a3148cb1f635fac2843917272c22
KhangBBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-05-23 04:26:03 -0700

    Prevent direct access to dbconnection.php

--- QuizCore/admin-temp/dbconnection.php
+++ QuizCore/admin-temp/dbconnection.php
@@ -1,11 +1,9 @@
 <?php
-// Define a constant to prevent direct access to files.
-define('DB_ACCESS', TRUE);
-
-// Include this file in all your scripts that require database connection.
-// Check if DB_ACCESS is not defined to prevent access to this file directly.
-if (!defined('DB_ACCESS')) {
-    die('Direct access not allowed');
+// In the individual PHP files, check if the constant indicating the application is defined.
+if (!defined('MY_APP')) {
+    // If the constant is not defined, redirect the user to the homepage and terminate the script.
+    header('Location: index.php');
+    exit;
 }
 
 const DB_SERVERNAME = "localhost";
