f6153d9745a8cf749eee61cf06d1dc8d2297332c
KhangBBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-05-23 20:51:14 -0700

    Move theme toggler to header.php

--- QuizCore/header.php
+++ QuizCore/header.php
@@ -179,6 +179,50 @@
     </symbol>
   </svg>
 
+  <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+    <button class="btn btn-secondary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
+      <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
+        <use href="#circle-half"></use>
+      </svg>
+      <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
+    </button>
+    <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
+      <li>
+        <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
+          <svg class="bi me-2 opacity-50" width="1em" height="1em">
+            <use href="#sun-fill"></use>
+          </svg>
+          Light
+          <svg class="bi ms-auto d-none" width="1em" height="1em">
+            <use href="#check2"></use>
+          </svg>
+        </button>
+      </li>
+      <li>
+        <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
+          <svg class="bi me-2 opacity-50" width="1em" height="1em">
+            <use href="#moon-stars-fill"></use>
+          </svg>
+          Dark
+          <svg class="bi ms-auto d-none" width="1em" height="1em">
+            <use href="#check2"></use>
+          </svg>
+        </button>
+      </li>
+      <li>
+        <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
+          <svg class="bi me-2 opacity-50" width="1em" height="1em">
+            <use href="#circle-half"></use>
+          </svg>
+          Auto
+          <svg class="bi ms-auto d-none" width="1em" height="1em">
+            <use href="#check2"></use>
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </div>
+
   <!-- Nav -->
   <nav class="navbar navbar-expand-md navbar-dark bg-red" aria-label="Fourth navbar example">
     <div class="container-fluid">
@@ -196,9 +240,6 @@
           <li class="nav-item">
             <a class="nav-link" href="./connection.php">Exam</a>
           </li>
-          <!-- <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li> -->
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Other</a>
             <ul class="dropdown-menu">
@@ -207,9 +248,6 @@
             </ul>
           </li>
         </ul>
-        <!--<button type="button" class="btn btn-light me-2" id="loginButton">Login / Sign-up</button> -->
       </div>
-
     </div>
-    <!-- <button type="button" class="btn btn-light me-2" id="loginButton">Login / Sign-up</button> -->
   </nav>
