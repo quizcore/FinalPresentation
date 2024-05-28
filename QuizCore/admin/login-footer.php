058f96d7686e3fa72c62184c9c3407fb956d0dc0
KhangBBBB <81422101+KhangBBBB@users.noreply.github.com>
2024-05-28 03:45:17 -0700

    Fix CWU link in admin footer

--- QuizCore/admin/login-footer.php
+++ QuizCore/admin/login-footer.php
@@ -10,7 +10,7 @@ if (!defined('MY_APP')) {
 <!--footer-->
 <div class="container-fluid py-5">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <a href="#" class="col-md-6 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <a href="https://www.cwu.edu/" class="col-md-6 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <img src="../img/cwu-logo-full-color-rgb-transparent.png" height="40px" />
         </a>
         <p class="col-md-6 mb-0 text-body-secondary">
