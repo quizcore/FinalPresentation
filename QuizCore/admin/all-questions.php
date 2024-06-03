299ab1a19c10cb0c1d3d3f053cf1f5dddd997015
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-06-03 00:11:12 -0700

    add a button to add a question on all-questions.php

--- QuizCore/admin/all-questions.php
+++ QuizCore/admin/all-questions.php
@@ -23,6 +23,7 @@ require_once 'header.php';
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">List of all questions</h1>
+    <a href="add-question.php" class="btn btn-lg btn-bd-red">Add A Question</a>
   </div>
 
   <!--Data table-->
